3db435d636e5da04231d125c8b251cffc8a1ad84
sabbir-hossen <sabbirakanda9535@gmail.com>
2024-07-31 10:18:42 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("yes").addEventListener("click",function(){
+document.getElementById("display").innerText="এইটা হয়তো আমার জন্য মঙ্গলকর";
+})
+document.getElementById("no").addEventListener("click",function(){
+    document.getElementById("display").innerText="I will marry you";
+})
